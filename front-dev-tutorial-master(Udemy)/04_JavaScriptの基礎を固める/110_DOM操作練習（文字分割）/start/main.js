f955eeb1af3cd0176adb8df81e0aa2e45f5083fc
Yu Sato <ym.74.mn@gmail.com>
2022-02-27 12:07:22 +0900

    DOMで文字アニメーション

---
+++ front-dev-tutorial-master(Udemy)/04_JavaScriptの基礎を固める/110_DOM操作練習（文字分割）/start/main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const el = document.querySelector('.animate-title');
+  // console.log(el.innerHTML.trim());
+  const str = el.innerHTML.trim().split("");
+
+  let concatStr = '';
+
+  // for(let c of str) {
+  //   c = c.replace(' ', '&nbsp;');
+  //   concatStr += `<span class="char">${c}</span>`;
+  // }
+
+
+  el.innerHTML =   str.reduce((acc, curr) => {
+    curr = curr.replace(' ', '&nbsp;');
+    return `${acc}<span class="char">${curr}</span>`;
+  }, "");
+  console.log(concatStr)
+});
